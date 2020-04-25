c49c82f60aa6e5bff8f28ce7a65e533032529b5a
Diego Zegarra <diego.zegarra79@icloud.com>
2020-04-25 20:18:55 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -7,12 +7,12 @@ def base_hash
 end
 
 def monopoly_with_second_tier
- base_hash[:railroads] = {
+ {railroads = {
     :pieces => 4,
     :rent_in_dollars =>{},
     :names =>{}
  }
- 
+ }
 end
 
 def monopoly_with_third_tier
